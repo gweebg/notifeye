471c2c0fa3954d62059462b799d6948412a19aba
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2025-05-03 20:24:21 +0100

    fix: disable body logging

--- lib/notifeye_web/endpoint.ex
+++ lib/notifeye_web/endpoint.ex
@@ -42,7 +42,7 @@ defmodule NotifeyeWeb.Endpoint do
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
-  plug NotifeyeWeb.Plugs.LogRequestBody
+  #plug NotifeyeWeb.Plugs.LogRequestBody
 
   plug Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
