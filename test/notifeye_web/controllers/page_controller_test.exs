f9cfbf6f379834d2a3658bdc38beb8639dc0f9ba
Guilherme Sampaio <guilherme.g.sampaio@gmail.com>
2025-08-17 22:17:08 +0100

    refactor: remove logzio api and web tests

--- test/notifeye_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule NotifeyeWeb.PageControllerTest do
-  use NotifeyeWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
